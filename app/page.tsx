53df95e60d5e93bb232efca4a0bd48de860e2a65
jamesecy604 <jamesecy604@gmail.com>
2025-01-04 17:02:04 -0800

    fix issues

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,6 @@ import dynamic from "next/dynamic";
  *
  * we're doing this because we're using a canvas element that can't be pre-rendered by Next.js on the server
  */
-const App = dynamic(() => import("./App"), { ssr: false });
+const App = dynamic(() => import("./App"), { ssr: true });
 
 export default App;
